bb1b69318b9082b2bbd526831d0d6436a5a83b12
Tor E Hagemann <hagemt@rpi.edu>
2014-01-12 22:53:58 -0800

    Add modifications to records (set/sort)

--- core/trie_records/sort.cc
+++ core/trie_records/sort.cc
@@ -1,14 +1,37 @@
+#include "insect/records.h"
+
+/* two strategies for record set creation: */
+
+#include "method1.ccc"
+#include "method2.ccc"
+
+#include <assert.h>
+
+static int
+__record_set_cmp(RecordSet *rs1, RecordSet *rs2, RecordComparator cmp)
+{
+	register int i1, i2, result;
+	assert(rs1 && rs2 && cmp);
+	result = rs1->size - rs2->size;
+	if (result) return result;
+	for (i1 = i2 = 0; i1 < rs1->size && i2 < rs2->size; ++i1, ++i2) {
+		result = cmp(rs1->data[i1], rs2->data[i2]);
+		if (result) return result;
+	}
+	return 0;
+}
+
 #include <stdlib.h>
 
-static RecordSet
-record_sorted(RecordComparator cmp)
+RecordSet *
+record_sort(RecordComparator cmp)
 {
 	RecordSet method1, method2, *record_set;
 	assert(cmp);
 	/* holds the final result */
 	record_set = malloc(sizeof(RecordSet));
 	assert(record_set);
-	record_set->size = slist_length(__record_set);
+	record_set->size = slist_length(__record_head);
 	/* use independent methods */
 	method1.size = method2.size = 0;
 	method1.data = method2.data = NULL;
@@ -17,12 +40,13 @@ record_sorted(RecordComparator cmp)
 	__to_array_then_qsort(&method2, cmp);
 	assert(method2.data && method2.size == record_set->size);
 	/* ensure the results are symmetrical */
-	if (__record_set_cmp(&method1, &method2)) {
+	if (__record_set_cmp(&method1, &method2, cmp)) {
 		free(record_set);
 		record_set = NULL;
 	} else {
 		record_set->data = method1.data;
 		free(method2.data);
 	}
+	record_set->cmp = cmp;
 	return record_set;
 }
--- include/insect/records.h
+++ include/insect/records.h
@@ -1,21 +1,49 @@
 #ifndef __INSECT_RECORDS_H__
 #define __INSECT_RECORDS_H__
 
+/* TODO(teh): use platform PATH_MAX */
+#define RECORD_PATH_MAX 4096
+typedef char *RecordPath;
+
 struct record_t {
+	RecordPath path;
 	int hits;
 };
 
 typedef struct record_t Record;
 
-void record_create(Record *);
-void record_destroy(Record *);
+extern Record *
+record_new(RecordPath);
 
-int record_count(void);
-void record_record(Record *);
+extern void
+record_free(Record *);
+
+extern int
+record_count(void);
+
+/* functional interface (visitors) */
 
 typedef void (*Visitor)(Record *);
-void record_each(Visitor);
 
-/* TODO(teh): sorted records */
+extern void
+record_each(Visitor);
+
+extern void
+record_purge(Visitor);
+
+/* sorted records (incomplete) */
+
+typedef int (*RecordComparator)(Record *, Record *);
+
+struct record_set_t {
+	int size;
+	Record **data;
+	RecordComparator cmp;
+};
+
+typedef struct record_set_t RecordSet;
+
+RecordSet *
+record_sort(RecordComparator);
 
 #endif /* __INSECT_RECORDS_H__ */
