fbe96411e7888bfb9cc92fe85e35e093239d24a7
Tor E Hagemann <hagemt@rpi.edu>
2014-01-16 05:13:26 -0800

    Add extern "C" wrapper to main header, for C++

--- include/insect.h
+++ include/insect.h
@@ -1,6 +1,10 @@
 #ifndef __INSECT_H__
 #define __INSECT_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* C++ */
+
 #include "insect/crawler.h"
 #include "insect/records.h"
 
@@ -10,5 +14,9 @@
 #ifdef USE_THREADS
 #include "insect/concurrent/thread.h"
 #endif /* USE_THREADS */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* C++ */
 
 #endif /* __INSECT_H__ */
